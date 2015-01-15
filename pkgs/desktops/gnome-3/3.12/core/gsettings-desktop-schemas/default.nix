cf8e1124abaeee7798d5f8935159e5854c364ec1
Tim Cuthbertson <tim@gfxmonk.net>
2015-01-16 09:29:36 +1100

    use correct default background path in gnome-desktop-schemas

--- pkgs/desktops/gnome-3/3.12/core/gsettings-desktop-schemas/default.nix
+++ pkgs/desktops/gnome-3/3.12/core/gsettings-desktop-schemas/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, intltool, glib, gobjectIntrospection
+{ stdenv, fetchurl, pkgconfig, intltool, glib, gobjectIntrospection, gnome_themes_standard
   # just for passthru
 , gtk3, gsettings_desktop_schemas }:
 
@@ -15,7 +15,15 @@ stdenv.mkDerivation rec {
     sha256 = "da75021e9c45a60d0a97ea3486f93444275d0ace86dbd1b97e5d09000d8c4ad1";
   };
 
-  buildInputs = [ glib gobjectIntrospection ];
+  buildInputs = [ glib gobjectIntrospection gnome_themes_standard ];
 
   nativeBuildInputs = [ pkgconfig intltool ];
+
+  # TODO: currently this is only used for embedding the default background,
+  # but could change / break in the future
+  postPatch = ''
+    for s in background screensaver; do
+      sed -i -e s'|@datadir@|${gnome_themes_standard}/share|' schemas/org.gnome.desktop.$s.gschema.xml.in.in
+    done
+  '';
 }
